656a0ba67063f2d50fce6b95e5f876c5ed564658
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-05-05 19:11:21 -0400

    configure the Request

--- Moviefy/Networking/Request.swift
+++ Moviefy/Networking/Request.swift
@@ -25,4 +25,31 @@ struct Request {
         "Content-Type":"Applicatoin/json",
         "Authorizatoin":"e876d92222f5e25dbca0fa994d4bafad"
     ]
+    
+    static func configureRequest(from route: Route, with parameters: [String: Any], and method: HTTPMethod, contains body: Data?) throws -> URLRequest {
+        
+        //safely unwrap URL or return error
+        guard let url = URL(string: "https://api.themoviedb.org/3/\(route.rawValue)") else { fatalError("Error while unwrapping url")}
+        var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10.0)
+        request.httpMethod = method.rawValue
+        request.httpBody = body
+        
+        try configureParametersAndHeaders(parameters: parameters, headers: headers, request: &request)
+        return request
+    }
+    
+    static func configureParametersAndHeaders(parameters: [String: Any]?,
+                                              headers: [String: String]?,
+                                              request: inout URLRequest) throws {
+        do {
+            if let headers = headers, let parameters = parameters {
+                try Encoder.encodeParameters(for: &request, with: parameters)
+                try Encoder.setHeaders(for: &request, with: headers)
+            }
+        } catch {
+            throw NetworkError.encodingFailed
+        }
+    }
+    
+    
 }
