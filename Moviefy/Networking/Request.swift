f871d057de04e63cb64e371fd8a70a7c69dd76f5
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-05-05 19:02:55 -0400

    Create HTTP methods and routes

--- Moviefy/Networking/Request.swift
+++ Moviefy/Networking/Request.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
+
+public enum Route: String{
+    case movies = "discover/movie"
+}
+
 struct Request {
     static let headers = [
         "Accept":"Application/json",
